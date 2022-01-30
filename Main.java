1dfe6bb5693399282e76a2867950f7d2c92cd01e
lubabik42 <lubabik@mail.ru>
2022-01-30 18:03:40 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
 package com.max.idea;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        double num = 3.1416;
-        byte num_1 = 127;
+    public static void main(String[] args)
+    {
+        int[] nums;
+        nums = new int[5];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 4;
+        nums[4] = 5;
+
+        int x = nums[0];
+        nums[0] = nums[nums.length-1];
+        nums[nums.length-1] = x;
+
+        System.out.println(nums[nums.length-3]+nums[nums.length-5]);
     }
 }
